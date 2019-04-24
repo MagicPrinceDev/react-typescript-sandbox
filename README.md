49837bf73705fd5d123fab0b725568e6fe0e0b96
GoldenStateFence <ferditutkun00@gmail.com>
2019-04-24 10:20:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-n4slji
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-n4slji)
---
+++ index.tsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+interface AppProps { }
+interface AppState {
+  name: string;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
